3f6ee73e80fee959e43dc456b3b1f9b99f7bfa87
oprienrico <repos@opri.it>
2020-10-25 17:00:16 -0400

    fixing resolvecoords for the case that there is only the right side

--- templates/electrode_models/ea_resolvecoords.m
+++ templates/electrode_models/ea_resolvecoords.m
@@ -17,9 +17,9 @@ if nargin==4
 end
 
 load([ea_getearoot,'templates',filesep,'electrode_models',filesep,options.elspec.matfname]);
-%for side=1:length(markers)
-for iside=1:length(options.sides)
-    side=options.sides(iside);
+for side=1:length(markers)%valid for unilateral support
+%for iside=1:length(options.sides)%does not cover all the use cases
+%    side=options.sides(iside);
 
     if resize
         can_dist=ea_pdist([electrode.head_position;electrode.tail_position]);
